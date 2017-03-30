8eb2acc48b3194ed1c3dd748dd4138105b476fe1
bartek8891 <bartek8891@gmail.com>
2017-03-30 16:00:17 +0200

    first commit

---
+++ views/site/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
+
+AppAsset::register($this);
+?>
+<p>
+    <?= Html::a('ZADANIE1', ['/task'], ['class' => 'btn btn-success'])
+//    . Html::beginForm(['/task'], 'post')
+        .Html::a('Panel administratora', ['/user/admin/index'], ['class' => 'btn btn-success'])
+    ?>
+</p>
